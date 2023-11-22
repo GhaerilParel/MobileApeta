95c185ca8fdf8d5bca98f5bd4b27c31f1e4a21c6
GhaerilParel <parelparel71@gmail.com>
2023-11-23 04:15:37 +0700

    perbaiki error

--- app/src/main/java/com/example/mobile_apeta/home.java
+++ app/src/main/java/com/example/mobile_apeta/home.java
@@ -21,31 +21,5 @@ public class home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(
-                new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        switch (item.getItemId()) {
-                            case R.id.home:
-                                Intent i = new Intent(home.this, home.class);
-                                startActivity(i);
-                                return true;
-                            case R.id.mitra:
-                                Intent i2 = new Intent(home.this, admin_awal.class);
-                                startActivity(i2);
-                                return true;
-                            case R.id.market:
-                                // Tindakan saat item "Market" dipilih
-                                // Ganti kode di sini sesuai dengan tindakan yang diinginkan
-                                return true;
-                            case R.id.profil:
-                                Intent i3 = new Intent(home.this, ubah_profil.class);
-                                startActivity(i3);
-                                return true;
-                        }
-                        return false;
-                    }
-                });
     }
 }
