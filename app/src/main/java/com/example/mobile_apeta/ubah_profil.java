6f325f90799677e8d5b7209c61acfdf9b726a911
GhaerilParel <parelparel71@gmail.com>
2023-11-22 15:24:59 +0700

    first commit

---
+++ app/src/main/java/com/example/mobile_apeta/ubah_profil.java
@@ -0,0 +1,14 @@
+package com.example.mobile_apeta;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ubah_profil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_profil);
+    }
+}
