35389553dd3c99e1e1c583da25997283ea521b16
GhaerilParel <parelparel71@gmail.com>
2023-11-24 00:43:24 +0700

    second commit

--- app/src/main/java/com/example/mobile_apeta/admin_awal.java
+++ app/src/main/java/com/example/mobile_apeta/admin_awal.java
@@ -1,6 +1,9 @@
 package com.example.mobile_apeta;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,15 @@ public class admin_awal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_awal);
+
+        Button btnPendaftaran = findViewById(R.id.btn1);
+
+        btnPendaftaran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(admin_awal.this, admin_awal.class);
+                startActivity(i);
+            }
+        });
     }
 }
