ee494b5b8c808ce48b36f09a2cebcbe98be9033c
dt46 <prezaa971@gmail.com>
2023-11-24 04:02:48 +0700

    admin ketiga belom

--- app/src/main/java/com/example/mobile_apeta/admin_ketiga.java
+++ app/src/main/java/com/example/mobile_apeta/admin_ketiga.java
@@ -1,10 +1,12 @@
 package com.example.mobile_apeta;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class admin_ketiga extends AppCompatActivity {
+    private EditText ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
